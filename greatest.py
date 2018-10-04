9df89f0468185c7a0eafcaad498b86ed2ab48830
Shivam Sapate <42692084+shivamsapate@users.noreply.github.com>
2018-10-04 23:01:11 +0530

    Add files via upload

---
+++ greatest.py
@@ -0,0 +1,11 @@
+n1 = int(input("enter the 1st number :-"))
+n2 = int(input("enter the 2nd number :-"))
+n3 = int(input("enter the 3rd number :-"))
+if n1 > n2:
+    if n1 > n3:
+        print("first number is greatest number")
+else:
+    if n2 > n3:
+        print("second number is greatest number") 
+    else:
+        print("third number is greatest ")
